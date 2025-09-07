0654a88023c48f26f4f15d9fc37c24a6f1547532
Denilson P. Guimarães <denilsonpautzguimaraes@gmail.com>
2025-09-07 17:59:01 -0300

    commit inicial

---
+++ src/main/java/com/denilson/trabalho/controllers/TarefaController.java
@@ -0,0 +1,66 @@
+package com.denilson.trabalho.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.denilson.trabalho.models.Tarefa;
+import com.denilson.trabalho.repositories.TarefaRepository;
+
+@RestController
+@RequestMapping("/tarefas")
+public class TarefaController {
+
+    @Autowired
+    private TarefaRepository repository;
+
+    @GetMapping("")
+    public List<Tarefa> listar() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> listarUnico(@PathVariable Long id) {
+        return repository.findById(id)
+            .map(record -> ResponseEntity.ok().body(record))
+            .orElse(ResponseEntity.badRequest().build());
+    }
+
+    @PostMapping("")
+    public Tarefa cadastro(@RequestBody Tarefa body) {
+        body.setId(0l);
+        return repository.save(body);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@PathVariable Long id, @RequestBody Tarefa body) {
+         return repository.findById(id)
+            .map(record -> {
+                record.setDataEntrega(body.getDataEntrega());
+                record.setNome(body.getNome());
+                record.setResponsavel(body.getResponsavel());
+                Tarefa updated = repository.save(record);
+                return ResponseEntity.ok().body(updated);
+            })
+            .orElse(ResponseEntity.badRequest().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> excluir(@PathVariable Long id) {
+        return repository.findById(id)
+        .map(record -> {
+            repository.deleteById(id);
+            return ResponseEntity.ok().build();
+        })
+        .orElse(ResponseEntity.badRequest().build());
+    }
+}
---
+++ src/main/java/com/denilson/trabalho/models/Tarefa.java
@@ -0,0 +1,55 @@
+package com.denilson.trabalho.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tarefa")
+public class Tarefa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    private LocalDate dataEntrega;
+
+    private String responsavel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataEntrega() {
+        return dataEntrega;
+    }
+
+    public void setDataEntrega(LocalDate dataEntrega) {
+        this.dataEntrega = dataEntrega;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String reposavel) {
+        this.responsavel = reposavel;
+    }
+}
